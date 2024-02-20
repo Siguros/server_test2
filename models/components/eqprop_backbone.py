Fix i_ext update sign & test cases

models/components/eqprop_backbone.py
@@ -487,10 +487,10 @@ class AnalogEP2(nn.Module):
             n_node = submodule.get_buffer("negative_node")
             res = 2 * (
                 torch.bmm(p_node.unsqueeze(2), self.prev_positive.unsqueeze(1))
-                .squeeze()
+                .reshape((p_node.size(0), *submodule.weight.shape))
                 .mean(dim=0)
                 - torch.bmm(n_node.unsqueeze(2), self.prev_negative.unsqueeze(1))
-                .squeeze()
+                .reshape((p_node.size(0), *submodule.weight.shape))
                 .mean(dim=0)
             )
             # broadcast to 2D

tests/eqprop/conftest.py
@@ -24,19 +24,23 @@ def second_order_strategy(toymodel) -> strategy.SecondOrderStrategy:
 
 @pytest.fixture(scope="module")
 def toy_backbone(cfg_train_global) -> AnalogEP2:
-    cfg = cfg_train_global.model.net.copy()
+    cfg = cfg_train_global.copy()
     HydraConfig().set_config(cfg)
     with open_dict(cfg):
-        cfg.batch_size = 1
-        cfg.dims = [2, 1, 1]
-        cfg.beta = 0.1
-        cfg.solver.max_iter = 20
-        cfg.solver.strategy.activation = {
+        # cfg.paths.out
+        cfg.model.net.batch_size = 1
+        cfg.model.net.dims = [2, 1, 1]
+        cfg.model.net.beta = 0.1
+        cfg.model.net.solver.strategy.add_nonlin_last = False
+        cfg.model.net.solver.strategy.clip_threshold = 1
+        cfg.model.net.solver.strategy.max_iter = 20
+        cfg.model.net.solver.strategy.activation = {
             "_target_": "src.eqprop.eqprop_util.SymReLU",
             "Vl": -0.6,
             "Vr": 0.6,
         }
-    backbone: AnalogEP2 = instantiate(cfg)
+    backbone_: AnalogEP2 = instantiate(cfg.model.net)
+    backbone = backbone_(hyper_params={"bias": True})
     backbone.model[0].weight.data = torch.tensor([[1.0, 1.0]])
     backbone.model[0].bias.data = torch.tensor([1.0])
     backbone.model[1].weight.data = torch.tensor([[2.0]])

tests/eqprop/test_eqprop.py
@@ -62,23 +62,33 @@ def test_eqprop_precision(cfg_train, ckpt_path):
 class TestAnalogEP2:
     @pytest.mark.parametrize("x", torch.tensor([[-1.0, -1.0]]))
     def test_free(self, toy_backbone, x):
-        ypred = toy_backbone.solver(x)
+        ypred = toy_backbone(x).detach()
         assert torch.allclose(ypred.squeeze(), torch.tensor(-1.2))
 
     @pytest.mark.parametrize("x", torch.tensor([[-1.0, -1.0]]))
     def test_nudge(self, toy_backbone, x):
+        toy_backbone.reset_nodes()
+        ypred = toy_backbone(x)
         criterion = torch.nn.MSELoss()
-        loss = criterion(toy_backbone.model.ypred, torch.tensor([0.0]))
+        loss = criterion(ypred, torch.tensor([0.3]))
         loss.backward()
-        assert torch.allclose(loss.grad, torch.tensor([-1.2]))
-        ypred = toy_backbone.solver(x, nudge_phase=True)
-        assert torch.allclose(ypred, [])
+        assert torch.allclose(ypred.grad, torch.tensor([-3.0]))
+        reversed_nodes, _ = toy_backbone.solver(x, nudge_phase=True)
+        assert torch.allclose(reversed_nodes[0], torch.tensor(-0.95))
+        assert torch.allclose(reversed_nodes[1], torch.tensor(-1.1))
 
-    @pytest.mark.parametrize("x", torch.tensor([[-1.0, -1.0]]))
+    @pytest.mark.parametrize("x", [[-1.0, -1.0]])
     def test_update(self, toy_backbone, x):
+        x = torch.tensor(x).reshape(1, -1)
+        toy_backbone.reset_nodes()
+        toy_backbone(x)
+        ypred = toy_backbone(x)
+        criterion = torch.nn.MSELoss()
+        loss = criterion(ypred, torch.tensor([0.3]))
+        loss.backward()
         toy_backbone.eqprop(x)
         net = toy_backbone.model
-        assert torch.allclose(net.lin1.weight.grad, torch.tensor([[0.0, 0.0]]))
-        assert torch.allclose(net.lin1.bias.grad, torch.tensor([0.0]))
-        assert torch.allclose(net.last.weight.grad, torch.tensor([[0.0]]))
-        assert torch.allclose(net.last.bias.grad, torch.tensor([0.0]))
+        assert torch.allclose(net.lin1.weight.grad, torch.tensor([[0.5, 0.5]]))
+        # assert torch.allclose(net.lin1.bias.grad, torch.tensor([0.0]))
+        assert torch.allclose(net.last.weight.grad.item(), torch.tensor([[0.225]]), atol=1e-4)
+        # assert torch.allclose(net.last.bias.grad, torch.tensor([0.0]))
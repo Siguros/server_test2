9b407ce229a439f5c09aaf3e1e933fd90548e13b
sinnce <ds6541@snu.ac.kr>
2024-08-19 09:37:16 +0900

    feat: Update EqPropBackbone to support configurable bias
    The EqPropBackbone class in wrapper.py has been updated to support configurable bias for each linear layer. The `bias` parameter has been added to the constructor, allowing the user to specify a boolean value or a list of booleans to enable or disable bias for each linear layer individually.

--- src/_eqprop/wrapper.py
+++ src/_eqprop/wrapper.py
@@ -11,6 +11,7 @@ class EqPropBackbone(nn.Module):
     def __init__(
         self,
         cfg: list[int] = [784 * 2, 128, 10 * 2],
+        bias: bool | list[bool] = [True, False],
         scale_input: int = 2,
         scale_output: int = 2,
         param_adjuster: Optional[eqprop_utils.AdjustParams] = eqprop_utils.AdjustParams(),
@@ -18,7 +19,8 @@ class EqPropBackbone(nn.Module):
         super().__init__()
         layers = []
         for idx in range(len(cfg) - 1):
-            layers.append(enn.EqPropLinear(cfg[idx], cfg[idx + 1]))
+            bias_idx = bias if isinstance(bias, bool) else bias[idx]
+            layers.append(enn.EqPropLinear(cfg[idx], cfg[idx + 1], bias=bias_idx))
         self.model = nn.Sequential(*layers)
         self.param_adjuster = param_adjuster
         eqprop_utils.interleave.set_num_input(scale_input)

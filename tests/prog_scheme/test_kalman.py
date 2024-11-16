fix: update dw_min value and reimplement taylor expansion at jacobian test

tests/prog_scheme/test_kalman.py
@@ -30,7 +30,7 @@ def test_linear_ekf(kf_analogtile, linear_ekf) -> None:
     tile.update(v, u, False)
     linear_ekf.predict(-torch.outer(u, v).flatten().numpy())
     assert get_persistent_weights(tile).flatten().numpy() == pytest.approx(
-        linear_ekf.x_est, abs=1e-3
+        linear_ekf.x_est, abs=5e-4
     )
 
 
@@ -39,7 +39,12 @@ def test_linear_ekf_jacobian(linear_ekf) -> None:
     dim = linear_ekf.dim
     x = torch.randn(dim).numpy()
     u = torch.randn(dim).numpy()
+    dx = du = (linear_ekf._scale_up + linear_ekf._scale_down) / 2
     linear_ekf.x_est = x
-    x_new = linear_ekf.f(x, u)
-    x_new_taylor = x + linear_ekf.f_jacobian_x(x, u) @ x + linear_ekf.f_jacobian_u(x, u) @ u
+    x_new = linear_ekf.f(x + dx, u + du)
+    x_new_taylor = (
+        linear_ekf.f(x, u)
+        + linear_ekf.f_jacobian_x(x, u) @ dx
+        + linear_ekf.f_jacobian_u(x, u) @ du
+    )
     assert x_new == pytest.approx(x_new_taylor, abs=1e-3)